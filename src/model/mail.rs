6ba17b34ea5e4b44e2adbce293f0b8f83c13bced
jocutajar <github-commit@robajz.info>
2018-07-08 09:55:17 +0200

    Added a structure to represent a request to accept a recipient

--- src/model/mail.rs
+++ src/model/mail.rs
@@ -1,12 +1,36 @@
 use model::command::*;
 use std::net::SocketAddr;
 
+/// Mail envelope before sending mail data
 #[derive(Debug)]
 pub struct Envelope {
+    /// Service name
     pub name: String,
-    pub peer: Option<SocketAddr>,
+    /// Local server endpoint
     pub local: Option<SocketAddr>,
+    /// Remote peer endpoint
+    pub peer: Option<SocketAddr>,
+    /// The SMTP helo sent by peer
     pub helo: Option<SmtpHelo>,
+    /// The SMTP mail from:path sent by peer
     pub mail: Option<SmtpMail>,
+    /// A list of SMTP rcpt to:path sent by peer
     pub rcpts: Vec<SmtpPath>,
+}
+
+/// Request to check if mail is accepted for given recipient
+#[derive(Debug)]
+pub struct AcceptRecipient {
+    /// Service name
+    pub name: String,
+    /// Local server endpoint
+    pub local: Option<SocketAddr>,
+    /// Remote peer endpoint
+    pub peer: Option<SocketAddr>,
+    /// The SMTP helo sent by peer
+    pub helo: Option<SmtpHelo>,
+    /// The SMTP mail from:path sent by peer
+    pub mail: Option<SmtpMail>,
+    /// The SMTP rcpt to:path sent by peer we want to check
+    pub rcpt: SmtpPath,
 }
