0920abb5573266e568602fbe9c1aca08a9ed07ee
Robert Cutajar <robert.cutajar@robajz.info>
2020-11-24 23:34:21 +0100

    Do not clean bin target samotop-server

--- clean.sh
+++ clean.sh
@@ -3,8 +3,8 @@
 # cleanups for CI caching
 
 du -shx target
-find target -name 'samotop*' -exec rm -rf {} \;
-find target -name 'libsamotop*' -exec rm -rf {} \;
+find target -name 'samotop*' -not -name samotop-server -exec rm -rf {} \;
+find target -name 'libsamotop*' -not -name samotop-server -exec rm -rf {} \;
 rm -rf target/debug/examples
 rm -rf target/debug/incremental
 rm -rf target/release/examples
