2a021dfa42f21227bf16a62deb122fe2b752ad1f
Robert Cutajar <robert.cutajar@robajz.info>
2021-03-10 22:30:08 +0100

    Adding timeout to spf

--- samotop-with-spf/src/lookup.rs
+++ samotop-with-spf/src/lookup.rs
@@ -1,17 +1,24 @@
-use async_std::task::block_on;
-use async_std_resolver::{config, proto::rr::rdata::MX, resolver, AsyncStdResolver, ResolveError};
+use async_std::{future::timeout, task::block_on};
+use async_std_resolver::{config, resolver, AsyncStdResolver, ResolveError};
 use std::{
+    future::Future,
     net::{IpAddr, Ipv4Addr, Ipv6Addr},
-    str,
+    time::Duration,
 };
 use trust_dns_resolver::error::ResolveErrorKind;
 use viaspf::{Lookup, LookupError, LookupResult, Name};
 
-pub struct TrustDnsResolver(AsyncStdResolver);
+pub struct TrustDnsResolver {
+    inner: AsyncStdResolver,
+    timeout: Duration,
+}
 
 impl TrustDnsResolver {
-    pub fn new(resolver: AsyncStdResolver) -> Self {
-        Self(resolver)
+    pub fn new(inner: AsyncStdResolver) -> Self {
+        Self {
+            inner,
+            timeout: Duration::from_secs(5),
+        }
     }
 }
 
@@ -27,21 +34,18 @@ pub async fn new_resolver() -> Result<TrustDnsResolver, ResolveError> {
 
 impl Lookup for TrustDnsResolver {
     fn lookup_a(&self, name: &Name) -> LookupResult<Vec<Ipv4Addr>> {
-        let fut = self.0.ipv4_lookup(name.as_str());
-        let res = block_on(fut);
-        Ok(res.map_err(to_lookup_error)?.into_iter().collect())
+        query_sync(self.timeout, self.inner.ipv4_lookup(name.as_str()))
+            .map(|lookup| lookup.into_iter().collect())
     }
 
     fn lookup_aaaa(&self, name: &Name) -> LookupResult<Vec<Ipv6Addr>> {
-        let fut = self.0.ipv6_lookup(name.as_str());
-        let res = block_on(fut);
-        Ok(res.map_err(to_lookup_error)?.into_iter().collect())
+        query_sync(self.timeout, self.inner.ipv6_lookup(name.as_str()))
+            .map(|lookup| lookup.into_iter().collect())
     }
 
     fn lookup_mx(&self, name: &Name) -> LookupResult<Vec<Name>> {
-        let fut = self.0.mx_lookup(name.as_str());
-        let res = block_on(fut);
-        let mut mxs: Vec<MX> = res.map_err(to_lookup_error)?.into_iter().collect();
+        let mut mxs: Vec<_> = query_sync(self.timeout, self.inner.mx_lookup(name.as_str()))
+            .map(|lookup| lookup.into_iter().collect())?;
         mxs.sort_by_key(|mx| mx.preference());
         mxs.into_iter()
             .map(|mx| {
@@ -51,25 +55,27 @@ impl Lookup for TrustDnsResolver {
     }
 
     fn lookup_txt(&self, name: &Name) -> LookupResult<Vec<String>> {
-        let fut = self.0.txt_lookup(name.as_str());
-        let res = block_on(fut);
-        res.map_err(to_lookup_error)?
-            .into_iter()
-            .map(|txt| {
-                txt.iter()
-                    .map(|data| str::from_utf8(data).map_err(|e| LookupError::Dns(Some(e.into()))))
-                    .collect()
-            })
-            .collect::<Result<_, _>>()
+        let txts: Vec<_> = query_sync(self.timeout, self.inner.txt_lookup(name.as_str()))
+            .map(|lookup| lookup.into_iter().collect())?;
+        Ok(txts.into_iter().map(|data| data.to_string()).collect())
     }
 
     fn lookup_ptr(&self, ip: IpAddr) -> LookupResult<Vec<Name>> {
-        let fut = self.0.reverse_lookup(ip);
-        let res = block_on(fut);
-        res.map_err(to_lookup_error)?
-            .into_iter()
+        let revs: Vec<_> = query_sync(self.timeout, self.inner.reverse_lookup(ip))
+            .map(|lookup| lookup.into_iter().collect())?;
+        revs.into_iter()
             .map(|name| Name::new(&name.to_ascii()).map_err(|e| LookupError::Dns(Some(e.into()))))
-            .collect::<Result<_, _>>()
+            .collect()
+    }
+}
+
+fn query_sync<T>(
+    time_out: Duration,
+    fut: impl Future<Output = Result<T, ResolveError>>,
+) -> LookupResult<T> {
+    match block_on(timeout(time_out, fut)) {
+        Ok(r) => r.map_err(to_lookup_error),
+        Err(_) => Err(LookupError::Timeout),
     }
 }
 
