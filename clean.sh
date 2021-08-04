3a1b087f81e00061cef2e6f79be7cda3db283f05
Robert Cutajar <robert.cutajar@robajz.info>
2021-08-04 10:16:33 +0200

    fixup smime

--- clean.sh
+++ clean.sh
@@ -3,12 +3,12 @@
 # cleanups for CI caching
 
 du -shx target
-find target -name 'samotop*' -not -name samotop-server -exec rm -rf {} \;
-find target -name 'libsamotop*' -not -name samotop-server -exec rm -rf {} \;
 rm -rf target/debug/examples
 rm -rf target/debug/incremental
 rm -rf target/release/examples
 rm -rf target/release/incremental
+find target -name 'samotop*' -not -name samotop-server -exec rm -rf {} \;
+find target -name 'libsamotop*' -not -name samotop-server -exec rm -rf {} \;
 cargo sweep -t 3
 cargo sweep -i
 du -shx target
